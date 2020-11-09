e6eb32cda002cfcc0a18206156ef077c1690b6cc
5kif4a <theololo057@gmail.com>
2020-11-09 23:59:14 +0600

    :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from 'react';
+import {Box, Typography} from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App: FC = () => {
+    return (
+        <Box>
+            <Typography variant="h1">
+                Lost N Found
+            </Typography>
+        </Box>
+    );
 }
 
 export default App;
