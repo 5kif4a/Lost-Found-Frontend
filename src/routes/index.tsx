927c6982a5b0c69514e6375f4adcdd4a8c216438
Akzhan <agosh.kz@mail.ru>
2020-11-10 15:33:25 +0600

    detail item

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,25 @@
-import React, {FC} from "react";
-import {Route, Switch} from "react-router-dom";
-import {Post} from "../pages/Post/Post";
-import {Feed} from "../pages/Feed/Feed";
-import {AuthContainer} from "../pages/Auth/AuthContainer";
-import {Items} from "../pages/Items/Items";
-import {_Stories} from "../pages/Stories/Stories";
-import {CreatePost} from "../pages/CreatePost/CreatePost";
+import React, { FC } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Post } from "../pages/Post/Post";
+import { Feed } from "../pages/Feed/Feed";
+import { AuthContainer } from "../pages/Auth/AuthContainer";
+import { Items } from "../pages/Items/Items";
+import { _Stories } from "../pages/Stories/Stories";
+import { CreatePost } from "../pages/CreatePost/CreatePost";
+import { DetailItem } from "../pages/DetailItem/DetailItem"
 
 export const Routes: FC = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={AuthContainer}/>
-            <Route exact path="/feed" component={Feed}/>
-            <Route exact path="/stories" component={_Stories}/>
-            <Route exact path="/create-post" component={CreatePost}/>
-            <Route exact path="/posts/:postId" component={Post}/>
-            <Route exact path="/me/items/:itemId" component={Items}/>
-            <Route exact path="/me/items" component={Items}/>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/" component={AuthContainer} />
+      <Route exact path="/feed" component={Feed} />
+      <Route exact path="/stories" component={_Stories} />
+      <Route exact path="/create-post" component={CreatePost} />
+      <Route exact path="/posts/:postId" component={Post} />
+      <Route exact path="/me/items/:itemId" component={DetailItem} />
+      <Route exact path="/me/items" component={Items} />
+  
+
+    </Switch>
+  );
 };
-
-
